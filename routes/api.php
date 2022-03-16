424d7b8558549994ad02793bf9691b4798552bc2
Rhys Evans <rhys.evans@redditchconnected.co.uk>
2022-03-16 10:59:30 +0000

    import controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CompanyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('companies', \App\Http\Controllers\Api\CompanyController::class);
+Route::apiResource('companies', CompanyController::class);
